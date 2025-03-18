c42db05403b9ce90ddcc24d8ff824decc0279252
Igor <igorviniciusfreitassouza@gmail.com>
2025-03-18 18:11:15 -0300

    Criação do componente input text

--- src/app/desing-system/ui-components/inputs/input-text/input-text.component.ts
+++ src/app/desing-system/ui-components/inputs/input-text/input-text.component.ts
@@ -1,26 +1,25 @@
-import { Component, Input, Output, EventEmitter, forwardRef, Self, Optional } from '@angular/core';
+import { Component, Input, forwardRef, Self, Optional } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from '../../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-input-text',
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.css'],
   standalone: true,
-  imports: [CommonModule],
- 
+  imports: [CommonModule, SvgIconComponent],
 })
 export class InputTextComponent implements ControlValueAccessor {
   @Input() placeholder: string = '';
-  @Input() alertText: string = 'Este campo é obrigatório';
+  @Input() alertText: string = '';
   @Input() isRequired: boolean = false;
-  
-  value: string = '';
-  disabled = false;
-  onChange: any = () => {};
-  onTouched: any = () => {};
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+  @Input() type: string = 'text';
 
-  // Updated constructor with @Self() and @Optional()
+  value: string = '';
+  isFocused: boolean = false;
+
   constructor(@Self() @Optional() public ngControl: NgControl) {
     if (this.ngControl) {
       this.ngControl.valueAccessor = this;
@@ -28,7 +27,7 @@ export class InputTextComponent implements ControlValueAccessor {
   }
 
   writeValue(value: string): void {
-    this.value = value;
+    this.value = value || '';
   }
 
   registerOnChange(fn: any): void {
@@ -39,14 +38,25 @@ export class InputTextComponent implements ControlValueAccessor {
     this.onTouched = fn;
   }
 
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
   onInputChange(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
     this.value = value;
     this.onChange(value);
+  }
+
+  onFocus(): void {
+    this.isFocused = true;
+  }
+
+  onBlur(): void {
+    this.isFocused = false;
     this.onTouched();
   }
+
+  shouldLabelFloat(): boolean {
+    return this.isFocused || !!this.value; // Flutua se estiver focado ou preenchido
+  }
+
+  onTouched = () => {};
+  onChange: any = () => {};
 }
