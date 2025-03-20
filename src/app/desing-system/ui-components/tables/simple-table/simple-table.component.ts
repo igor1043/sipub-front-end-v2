4658f3b670117269d60fb1b82a49104b45ccbf07
Igor <igorviniciusfreitassouza@gmail.com>
2025-03-20 17:43:48 -0300

    Ajustes no componente simple table

--- src/app/desing-system/ui-components/tables/simple-table/simple-table.component.ts
+++ src/app/desing-system/ui-components/tables/simple-table/simple-table.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input, Output, EventEmitter, ViewChild, AfterViewInit, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, SimpleChanges, OnChanges } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule, PageEvent, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSort, MatSortModule } from '@angular/material/sort'; 
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { CustomPaginatorIntl } from './custom-paginator-intl';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -19,43 +18,43 @@ export interface ColumnConfig {
   selector: 'app-simple-table',
   standalone: true,
   imports: [
-    MatTableModule, 
-    MatPaginatorModule, 
-    MatButtonModule, 
-    MatIconModule, 
-    MatCheckboxModule, 
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
     CommonModule,
-    MatSortModule,MatFormFieldModule,MatInputModule
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './simple-table.component.html',
   styleUrls: ['./simple-table.component.css'],
   providers: [{ provide: MatPaginatorIntl, useClass: CustomPaginatorIntl }]
 })
-export class SimpleTableComponent<T extends { id: any }> implements AfterViewInit, OnChanges {
+export class SimpleTableComponent<T extends { id: any }> implements OnChanges {
   @Input() data: T[] = [];
   @Input() columns: ColumnConfig[] = [];
   @Input() pageSizeOptions: number[] | undefined;
   @Input() selectedItem: T | null = null;
-  @Input() sortingEnabled: boolean = true; // Parâmetro para controlar o sorting
+  @Input() sortingEnabled: boolean = true;
 
   @Output() onAdd = new EventEmitter<void>();
   @Output() onEdit = new EventEmitter<T>();
   @Output() onDelete = new EventEmitter<T>();
   @Output() onSelect = new EventEmitter<T>();
   @Output() onPageChange = new EventEmitter<PageEvent>();
-  @Output() onSelectedItemsChange = new EventEmitter<T[]>(); 
+  @Output() onSelectedItemsChange = new EventEmitter<T[]>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort; // Referência ao MatSort
+  @ViewChild(MatSort) sort!: MatSort;
 
   displayedColumns: string[] = [];
   dataSource = new MatTableDataSource<T>();
-
   selectedItems = new Set<any>();
   isAllSelected = false;
-  showFilter: boolean = false; // Controle de visibilidade do campo de pesquisa
+  showFilter = false;
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['columns']) {
       this.displayedColumns = ['select', ...this.columns.map(c => c.key), 'actions'];
     }
@@ -65,14 +64,13 @@ export class SimpleTableComponent<T extends { id: any }> implements AfterViewIni
     }
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    if (this.sortingEnabled) {
-      this.dataSource.sort = this.sort; // Ativa o sorting se estiver habilitado
-    }
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  toggleSelection(row: T): void {
+  toggleSelection(row: T, event: MouseEvent): void {
+    event.stopPropagation();
     if (this.selectedItems.has(row.id)) {
       this.selectedItems.delete(row.id);
     } else {
@@ -86,7 +84,8 @@ export class SimpleTableComponent<T extends { id: any }> implements AfterViewIni
     return this.selectedItems.has(row.id);
   }
 
-  toggleSelectAll(): void {
+  toggleSelectAll(event: MouseEvent): void {
+    event.stopPropagation();
     if (this.isAllSelected) {
       this.selectedItems.clear();
     } else {
@@ -96,18 +95,19 @@ export class SimpleTableComponent<T extends { id: any }> implements AfterViewIni
     this.emitSelectedItems();
   }
 
-  updateSelectAllState(): void {
-    this.isAllSelected = this.dataSource.data.length > 0 && this.selectedItems.size === this.dataSource.data.length;
+  clearSelections(): void {
+    this.selectedItems.clear();
+    this.updateSelectAllState();
+    this.emitSelectedItems();
   }
 
-  emitSelectedItems(): void {
+  private updateSelectAllState(): void {
+    this.isAllSelected = this.dataSource.data.length > 0 && 
+                       this.selectedItems.size === this.dataSource.data.length;
+  }
+
+  private emitSelectedItems(): void {
     const selectedItemsArray = this.dataSource.data.filter(row => this.selectedItems.has(row.id));
     this.onSelectedItemsChange.emit(selectedItemsArray);
-  }
-
-  // Método para aplicar o filtro
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
