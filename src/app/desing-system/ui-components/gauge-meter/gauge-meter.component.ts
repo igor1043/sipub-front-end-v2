c22b3578aecd0cd5a3eb18733135f6da9737cadb
Igor <igorviniciusfreitassouza@gmail.com>
2025-04-19 10:54:04 -0300

    Ajustes no componente de medidor

--- src/app/desing-system/ui-components/gauge-meter/gauge-meter.component.ts
+++ src/app/desing-system/ui-components/gauge-meter/gauge-meter.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gauge-meter',
   templateUrl: './gauge-meter.component.html',
-  styleUrls: ['./gauge-meter.component.scss']
+  styleUrls: ['./gauge-meter.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class GaugeMeterComponent implements OnInit {
   @Input() value = 0; // valor entre 0 e 100
@@ -12,7 +15,10 @@ export class GaugeMeterComponent implements OnInit {
   needleX = 100;
   needleY = 50; // Posição inicial mais alta (12 horas)
 
+  ticks: { x: number; y: number; label: number }[] = [];
+
   ngOnInit() {
+    this.generateTicks();
     this.animateNeedle();
   }
 
@@ -39,4 +45,19 @@ export class GaugeMeterComponent implements OnInit {
     this.needleX = 100 + length * Math.cos(radians);
     this.needleY = 100 + length * Math.sin(radians);
   }
+  generateTicks() {
+  const radius = 70;
+  const centerX = 100;
+  const centerY = 100;
+
+  for (let value = 0; value <= 100; value += 20) {
+    const angle = (value / 100) * 180 - 180;
+    const radians = (angle * Math.PI) / 180;
+
+    const x = centerX + (radius - 10) * Math.cos(radians);
+    const y = centerY + (radius - 10) * Math.sin(radians);
+
+    this.ticks.push({ x, y, label: value });
+  }
+}
 }
